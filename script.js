a1ff71ff649193f7e0c35ec9f246ebb2a4e15739
anhkhoinguyen1310 <anhkhoinguyen1310@gmail.com>
2023-10-07 16:21:58 -0400

    adding the mainPage

--- script.js
+++ script.js
@@ -3,20 +3,8 @@ document.getElementById('sign-in-button').addEventListener("click", function()
     const userName = document.getElementById('username').value;
     console.log("clicked");
    	document.getElementById('username').value = '';
-	document.getElementById('sign-in').style.display = 'none';
-	document.getElementById('sign-out').style.display = 'block';
-	const displayUsername = document.getElementById('displayUsername'); //display userName + logo after signing in
-	displayUsername.innerHTML = 'Hello ' + userName; //innerHTML normally use to add st to the element.
-	
+    localStorage.setItem('username', userName);
+    location.href = "/mainPage.html";
+    console.log(location)
 })
 
-document.getElementById('sign-out-button').addEventListener('click', signOut)
-function signOut(e)
-{
-    document.getElementById('sign-in').style.display = 'block'; 
-    document.getElementById('sign-out').style.display = 'none'; 
-	const storeApplicastionState = JSON.stringify(applicationState);
-	localStorage.setItem('class', storeApplicastionState);
-	//2. translate back to JS object. 
-
-}
---
+++ scriptForMainPage.js
@@ -0,0 +1,13 @@
+//const userName = document.getElementById('username');
+const username = localStorage.getItem('username');
+const displplayUsername = document.getElementById('displayUsername');
+displayUsername.innerHTML = 'Hello ' + username;
+console.log(username);
+document.getElementById('sign-out-button').addEventListener('click', signOut)
+function signOut(e)
+{
+    console.log('clicked');
+    window.location.href = "/index.html";
+    localStorage.getItem()
+
+}  
